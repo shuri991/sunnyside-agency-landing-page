55ae1aeea760ae9b182779a78a51831c6d8ada16
ShurikoAlzate <alzateshuriko@gmail.com>
2022-02-23 08:51:52 -0500

    Añadiendo archivos y creando el header

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,76 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+ <div>
+     <img class="image-header" alt="Imagen-header" src="../src/assets/image/image-header.jpg" >
+     <Header/>
+     <div class="figure">
+       <img src="../src/assets/icon-arrow-down.svg" alt="arrow down">
+     </div>
+     
+   <!-- <div id="main-container">
+     <h2>Todo contenido</h2>
+   </div> -->
+ </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/header.vue';
+// import Main from './components/main.vue';
+// import Footer from './components/footer.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Header 
+    // Main, 
+    // Footer
   }
+  // data() {
+  //   return {
+      /*Aqui van los elementos de javaScript, para que sea funcional*/
+  //   }
+  // },
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+:root{
+  /* ## Colors */
+
+/* ### Primary */
+
+--Soft-red: hsl(7, 99%, 70%);
+--Yellow: hsl(51, 100%, 49%);
+--Dark-desaturated-cyan: hsl(167, 40%, 24%); /*(graphic design text)*/
+--Dark-blue: hsl(198, 62%, 26%); /*(photography text) */
+--Dark-moderate-cyan : hsl(168, 34%, 41%); /*(footer)*/
+
+/* ### Neutral */
+
+--Very-dark-desaturated-blue: hsl(212, 27%, 19%);
+--Very-dark-grayish-blue: hsl(213, 9%, 39%);
+--Dark-grayish-blue: hsl(232, 10%, 55%);
+--Grayish-blue: hsl(210, 4%, 67%);
+--White: hsl(0, 0%, 100%);
 }
+
+ *{
+   box-sizing: border-box;
+   margin: 0;
+   font-size: 18px;
+ }
+ .image-header{
+   width: 100%;
+   position: absolute;
+ }
+ .figure{
+   position: relative;
+   display: flex;
+   margin-top: 60px;
+   justify-content: center;
+}
+
+
+ 
+
 </style>
---
+++ src/components/header.vue
@@ -0,0 +1,74 @@
+<template>
+    <div class="header">
+        <h2>sunnyside</h2>
+        <ul>
+            <li>About</li>
+            <li>Services</li>
+            <li>Projects</li>
+            <!-- <li class="contact">Contact</li> -->
+        </ul>
+        <button class="contact">Contact</button>
+    </div>
+
+    <div class="main-title">
+       <h1>we are creatives</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'header',
+    props: {
+        msg: String
+    }
+}
+</script>
+
+<style scoped>
+
+.header{
+    display: flex;
+    justify-content: end;
+    position: relative;
+    padding: 40px;
+}
+
+h2{
+    color: white;
+    width: 100%;
+}
+
+ul{
+    display: flex;
+    justify-content: end;
+    list-style: none;
+}
+
+li{
+     color: white;
+     margin-right: 50px;
+}
+
+.contact{
+    background: white;
+    color: black;
+    
+}
+
+.main-title{
+    position: relative;
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    margin-top: 50px;
+}
+.main-title h1{
+    color: white;
+    font-family:'Fraunces', serif;
+    font-weight: 900;
+    font-size: 5rem ;
+}
+
+
+
+</style>
